f18f90efc7b63bb95388bb8035876f677ff9dc05
YesNickto <112197292+YesNickto@users.noreply.github.com>
2024-11-08 23:35:34 +0300

    Update task_2.py

--- Lab 3/task_2.py
+++ Lab 3/task_2.py
@@ -1,13 +1,12 @@
-def find_common_participants(group1, group2, delimiter = "|"):
+def find_common_participants(group1, group2, delimiter=","):
     group1_set = set(group1.split(delimiter))
     group2_set = set(group2.split(delimiter))
-    common_participants = list(group1_set.intersection(group2_set))
+    common_participants = sorted(list(group1_set.intersection(group2_set)))
     return common_participants
 
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
-# Проверка с разделителем "|" (используется значение по умолчанию)
-common = find_common_participants(participants_first_group, participants_second_group)
-print(f"Общие участники: {common}")
+common = find_common_participants(participants_first_group, participants_second_group, delimiter="|")
+print(f"Общие участники : {common}")
